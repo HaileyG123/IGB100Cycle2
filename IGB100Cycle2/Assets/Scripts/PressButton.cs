c738f57112eec5967efae2547a84c6f34ba8aada
HaileyG123 <130008625+HaileyG123@users.noreply.github.com>
2023-04-16 18:39:40 +1000

    Pressing Buttons
    script to press buttons and open door

---
+++ IGB100Cycle2/Assets/Scripts/PressButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButton : MonoBehaviour
+{
+    bool buttonPressed = false;
+    public GameObject door;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(UnityEngine.Collision other)
+    {
+        if (other.gameObject.tag == "Button" && buttonPressed == false)
+        {
+            buttonPressed = true;
+            Destroy(door);
+            //Debug.Log("Button Pressed");
+        }
+    }
+}
