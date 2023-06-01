6ee7d9af064b48e4a60c2d67e96fd9079ce85e92
HaileyG123 <130008625+HaileyG123@users.noreply.github.com>
2023-06-01 11:03:16 +1000

    room 1 all transferred

--- IGB100Cycle2/Assets/Scripts/GameManager/Timer.cs
+++ IGB100Cycle2/Assets/Scripts/GameManager/Timer.cs
@@ -14,6 +14,7 @@ public class Timer : MonoBehaviour
     void Start()
     {
         TimerOn = true;
+        //TimerText = GameObject.Find("Text (TMP)");
     }    
 
     // Update is called once per frame
